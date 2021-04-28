3bf32685981aedae16b31f56bf595d6f72e82810
nadia <nadcarcha@gmail.com>
2021-04-28 12:32:30 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { CounterComponent } from './counter/counter.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CounterComponent,
+    PlaygroundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
